201ff343cc8c66f2dde93b14af5b10ffc0c79bf2
julia-richards <julia.m.richards@gmail.com>
2020-10-27 14:17:42 -0600

    added user login and registration

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 
 const bookRoutes = require('./routes/book');
+const userRoutes = require('./routes/user')
 
 const app = express();
 
@@ -12,7 +13,7 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bookRoutes);
-
+app.use(userRoutes)
 // Catch unhandled requests and forward to error handler.
 app.use((req, res, next) => {
   const err = new Error('The requested page couldn\'t be found.');
--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,10 @@ const { check, validationResult } = require('express-validator');
 
 const db = require('../db/models');
 const { csrfProtection, asyncHandler } = require('./utils');
-
+const bcrypt = require('bcryptjs')
 const router = express.Router();
+
+
 
 router.get('/user/register', csrfProtection, (req, res) => {
   const user = db.User.build();
@@ -16,7 +18,49 @@ router.get('/user/register', csrfProtection, (req, res) => {
 });
 
 const userValidators = [
-  // TODO Define the user validators.
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for First Name')
+    .isLength({ max: 50 })
+    .withMessage('First Name must not be more than 50 characters long'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Last Name')
+    .isLength({ max: 50 })
+    .withMessage('Last Name must not be more than 50 characters long'),
+  check('emailAddress')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address')
+    .isLength({ max: 255 })
+    .withMessage('Email Address must not be more than 255 characters long')
+    .isEmail()
+    .withMessage('Email Address is not a valid email')
+    .custom((value) => {
+      return db.User.findOne({ where: { emailAddress: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Email Address is already in use by another account');
+          }
+        });
+    }),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password')
+    .isLength({ max: 50 })
+    .withMessage('Password must not be more than 50 characters long')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+    .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+  check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Confirm Password')
+    .isLength({ max: 50 })
+    .withMessage('Confirm Password must not be more than 50 characters long')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Confirm Password does not match Password');
+      }
+      return true;
+    })
 ];
 
 router.post('/user/register', csrfProtection, userValidators,
@@ -37,6 +81,8 @@ router.post('/user/register', csrfProtection, userValidators,
     const validatorErrors = validationResult(req);
 
     if (validatorErrors.isEmpty()) {
+      const hashedPassword = await brcyrpt.hash(password, 10)
+      user.hashedPassword = hashedPassword;
       await user.save();
       res.redirect('/');
     } else {
@@ -48,6 +94,64 @@ router.post('/user/register', csrfProtection, userValidators,
         csrfToken: req.csrfToken(),
       });
     }
+  }));
+
+  router.get('/user/login', csrfProtection, (req, res) => {
+
+    res.render('user-login',{
+      title: 'Login',
+      csrfToken: req.csrfToken()
+  })
+  })
+
+  const loginValidators = [
+    check('emailAddress')
+      .exists({checkFalsy: true})
+      .withMessage('Please provide a value for Email Address'),
+    check('password')
+      .exists({checkFalsy: true})
+      .withMessage('Please provide a value for Password'),
+  ];
+
+  router.post('/user/login', csrfProtection, loginValidators,
+  asyncHandler(async (req, res) => {
+    const {
+      emailAddress,
+      password,
+    } = req.body;
+
+    let errors = [];
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      // Attempt to get the user by their email address.
+      const user = await db.User.findOne({ where: { emailAddress } });
+
+      if (user !== null) {
+        // If the user exists then compare their password
+        // to the provided password.
+        const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+
+        if (passwordMatch) {
+          // If the password hashes match, then login the user
+          // and redirect them to the default route.
+          // TODO Login the user.
+          return res.redirect('/');
+        }
+      }
+
+      // Otherwise display an error message to the user.
+      errors.push('Login failed for the provided email address and password');
+    } else {
+      errors = validatorErrors.array().map((error) => error.msg);
+    }
+
+    res.render('user-login', {
+      title: 'Login',
+      emailAddress,
+      errors,
+      csrfToken: req.csrfToken(),
+    });
   }));
 
 module.exports = router;
